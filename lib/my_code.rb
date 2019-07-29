94d9126f6efc77618e29a07c7db7a701a18a18b6
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-29 01:58:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,8 +6,7 @@ def map(array)
     return array
   end
   
-  def map_to_double(source_array)
-  source_array.map { |n| n * 2 }
+  def map_to_double(source_array){ |n| n * 2 }
   end
 
   def map_to_square(source_array){ |n| n * n}
